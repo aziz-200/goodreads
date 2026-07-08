77a10537e48036f7662125d6d9a74afa06308ca9
Azizbek Axtamov <azizaxtamov0201@gmail.com>
2026-07-08 22:08:06 +0500

    models and model forms are written:

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from books.models import (Book,
+                          Author,
+                          BookAuthor,
+                          BookReview)
 
-# Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    search_fields = ["title", 'isbn']
+    # list_filter = ['title',"isbn"] bu yerda kk emas ammo filter qilish uchun ishlatiladi
+    list_display = ['title', 'isbn']
+
+class AuthorAdmin(admin.ModelAdmin):
+    search_fields = ['first_name', 'last_name']
+    list_display = ['first_name', 'last_name']
+
+class BookAuthorAdmin(admin.ModelAdmin):
+    search_fields = ["book", 'author']
+    list_display = ['book', 'author']
+    list_filter = ['book', 'author']
+
+class BookReviewAdmin(admin.ModelAdmin):
+    search_fields = ['stars_given']
+    list_display = ['stars_given', 'book']
+    list_filter = ['stars_given']
+
+
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(BookAuthor, BookAuthorAdmin)
+admin.site.register(BookReview, BookReviewAdmin)
---
+++ users/tests.py
@@ -0,0 +1,4 @@
+from django.test import TestCase
+
+class RegisterationTest(TestCase):
+    def test_user_account_is_created(self):
