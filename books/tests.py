72cb0886fc53d00ddae60aa705223ddd1897aa46
Azizbek Axtamov <azizaxtamov0201@gmail.com>
2026-07-09 11:08:40 +0500

    Book create and lists written :

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from books.models import Book
+
+
+class BooksTest(TestCase):
+    def test_no_books(self):
+        response = self.client.get(reverse('books:list'))
+
+        self.assertContains(response, 'No books found')
+
+    def test_books_list(self):
+        Book.objects.create(title='book1',  isbn='313451', description='description1')
+        Book.objects.create(title='book2',  isbn='123451', description='description2')
+        Book.objects.create(title='book3',  isbn='123234', description='description3')
+
+        response = self.client.get(reverse('books:list'))
+        books = Book.objects.all()
+        for book in books:
+            self.assertContains(response, book.title)
+
+    def test_book_detail(self):
+        book = Book.objects.create(title='book1', isbn='313451', description='description1')
+        response = self.client.get(reverse('books:detail', kwargs={'id':book.id}))
+
+        self.assertContains(response, book.title)
+        self.assertContains(response, book.description)
