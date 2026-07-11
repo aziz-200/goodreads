130e756da53374db80c2b47bcf147cf5c2453612
Azizbek Axtamov <azizaxtamov0201@gmail.com>
2026-07-11 16:01:24 +0500

    email sending is completed:

---
+++ users/signals.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import User
+from django.core.mail import send_mail
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
+from users.models import CustomUser
+
+
+@receiver(post_save, sender=CustomUser)
+def send_welcome_email(sender, instance, created, **kwargs):
+        print(f"created: {created}")
+        if created:
+            print("created")
+            send_mail(
+                'Welcome to GoodReads',
+                f"Hi, {instance.username}! Welcome to GoodReads.",
+                'azizaxtamov0201@gmail.com',
+                [instance.email]
+
+            )
